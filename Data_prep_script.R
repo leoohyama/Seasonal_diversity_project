784092814fca06641afba5ffd3c2aefc1b927d47
leoohyama <leo.ohyama@ufl.edu>
2021-05-12 16:22:16 -0400

    null models inclusion

--- Data_prep_script.R
+++ Data_prep_script.R
@@ -68,7 +68,12 @@ trait_table_all$FRich<-NA
 trait_table_all$FEve<-NA
 trait_table_all$FDiv<-NA
 trait_table_all$jost<-NA
-trait_table_all$mean_Frich_Null<-NA
+trait_table_all$mean_FRich_Null<-NA
+trait_table_all$mean_FEve_Null<-NA
+trait_table_all$mean_FDiv_Null<-NA
+trait_table_all$sd_FRich_Null<-NA
+trait_table_all$sd_FEve_Null<-NA
+trait_table_all$sd_FDiv_Null<-NA
 
 for(i in 1:length(time_stamp_list)){
   
@@ -111,11 +116,11 @@ for(i in 1:length(time_stamp_list)){
   
   #rescale trait data
   trait_data = scale(time1_traits)
-  library(vegan)
+  
   trait_matrix = vegan::vegdist(trait_data,"euclidean")
   branch_lengths<-hclust(trait_matrix,"average")
   branch_lengths
-  library(picante)
+ 
   trait_tree = ape::as.phylo(branch_lengths)
   plot(trait_tree)
   str(time1_comm)
@@ -129,9 +134,7 @@ for(i in 1:length(time_stamp_list)){
     trait_table_all$FD_veg[trait_table_all$Site == paste(vegan_tbl$Site[j]) &
                              trait_table_all$time_code == time_stamp_list[i]]<-vegan_tbl$FD[j]
   }
-  #ses
-  ses.mpd.traits = picante::ses.mpd(time1_comm, trait_matrix, null.model = "independentswap",
-                                    abundance.weighted = FALSE, runs = 999)
+  
   
   #calculate the ses of functional indices for 1000 null models
 
@@ -142,24 +145,55 @@ for(i in 1:length(time_stamp_list)){
   #first create null model datset to hold all the values
   null_model_results<-data.frame(run = rep(1:1000, each =length(rownames(time1_comm))),
                                  Site = rep(rownames(time1_comm), 1000), FRich.Null = NA,
-                                 FEve.Null = NA, FDiv.Null = NA, ID = seq(1:1000))
+                                 FEve.Null = NA, FDiv.Null = NA, ID = seq(1:1000),
+                                 Time = rep(time_stamp_list[i], 1000))
   for (b in 1:1000) {
+    nullmatrix<-time1_comm
+    nullmatrix[nullmatrix >0] <- 1
     randommatrixswap<-randomizeMatrix(time1_comm, null.model = c("independentswap"), iterations = 1000)
     randommatrixswap
-    #then you calculate calculate functional metrics with randomized comm data (need to do this 1000 times)
+    #then you calculate calculate functional metrics with randomized comm data for each time
+    #period(need to do this 1000 times). 
     trial<-dbFD(time1_traits, randommatrixswap, w.abun =T,calc.FDiv =T)
     func_rich_metrics<-data.frame(Site = rownames(data.frame(trial)), FRic = trial$FRic,
                                   FEve = trial$FEve, FDiv = trial$FDiv, 
-                                  Run = rep(paste(b), length(rownames(data.frame(trial)))))
+                                  Run = rep(paste(b), length(rownames(data.frame(trial))))
+                                  )
     
     for(a in 1:nrow(func_rich_metrics)){
       null_model_results$FRich.Null[null_model_results$run == paste(b) &
                                       null_model_results$Site == paste(func_rich_metrics$Site[a])]<-func_rich_metrics$FRic[a]
-      
+      null_model_results$FEve.Null[null_model_results$run == paste(b) &
+                                      null_model_results$Site == paste(func_rich_metrics$Site[a])]<-func_rich_metrics$FEve[a]
+      null_model_results$FDiv.Null[null_model_results$run == paste(b) &
+                                      null_model_results$Site == paste(func_rich_metrics$Site[a])]<-func_rich_metrics$FDiv[a]
+      null_model_results$time[null_model_results$run == paste(b) &
+                                   null_model_results$Site == paste(func_rich_metrics$Site[a])]<-func_rich_metrics$FDiv[a]
     }
     
   }
+  null_summary<-null_model_results %>% group_by(Site) %>%
+    summarise(mean_FRich.null = mean(FRich.Null, na.rm =T),
+              sd_FRich.null = sd(FRich.Null, na.rm =T),
+              mean_FEve.null = mean(FEve.Null, na.rm = T),
+              sd_FEve.null = sd(FEve.Null, na.rm =T),
+              mean_FDiv.null = mean(FDiv.Null, na.rm = T),
+              sd_FDiv.null = sd(FDiv.Null, na.rm =T))
 
+  for(z in 1:nrow(null_summary)){
+    trait_table_all$mean_FRich_Null[trait_table_all$Site == paste(null_summary$Site[z]) &
+                                      trait_table_all$time_code == time_stamp_list[i]]<-null_summary$mean_FRich.null[z]
+    trait_table_all$mean_FEve_Null[trait_table_all$Site == paste(null_summary$Site[z]) &
+                                      trait_table_all$time_code == time_stamp_list[i]]<-null_summary$mean_FEve.null[z]
+    trait_table_all$mean_FDiv_Null[trait_table_all$Site == paste(null_summary$Site[z]) &
+                                      trait_table_all$time_code == time_stamp_list[i]]<-null_summary$mean_FDiv.null[z]
+    trait_table_all$sd_FRich_Null[trait_table_all$Site == paste(null_summary$Site[z]) &
+                                     trait_table_all$time_code == time_stamp_list[i]]<-null_summary$sd_FRich.null[z]
+    trait_table_all$sd_FEve_Null[trait_table_all$Site == paste(null_summary$Site[z]) &
+                                     trait_table_all$time_code == time_stamp_list[i]]<-null_summary$sd_FEve.null[z]
+    trait_table_all$sd_FDiv_Null[trait_table_all$Site == paste(null_summary$Site[z]) &
+                                   trait_table_all$time_code == time_stamp_list[i]]<-null_summary$sd_FDiv.null[z]
+  }
 
   
   ##########################
@@ -183,6 +217,16 @@ for(i in 1:length(time_stamp_list)){
   
   print(paste(i, "has finished", " "))
 }
+
+
+
+
+
+
+
+
+
+
 
 trait_table_all %>% mutate(difference = mpd.obs-mpd.rand.mean) %>%
   group_by(time_code) %>%
@@ -242,6 +286,10 @@ for(i in 1:nrow(facet_data)){
     }
   }
 }  
+#getting mean values for null model results:
+
+
+
 #this is a key for the geom_rect, displays values for each month
 data.frame(date = unique(facet_data$time_code), 
            code = as.numeric(unique(facet_data$time_code)))
@@ -299,6 +347,48 @@ ggplot(data = facet_data) +
         strip.text = element_text(size = 10, face = "bold"))
 
 
+####facet graph by ses of functional metrics
+SES.data<-trait_table_all %>%
+  mutate(SES_FRich = (mean(FRich, na.rm = T)- mean(mean_FRich_Null, na.rm = T))/sd_FRich_Null,
+         SES_FDiv = (mean(FDiv,na.rm = T)- mean(mean_FDiv_Null,na.rm = T))/sd_FDiv_Null,
+         SES_FEve = (mean(FEve,na.rm = T)- mean(mean_FEve_Null,na.rm = T))/sd_FEve_Null) %>% 
+  dplyr::select(time_code, Site, SES_FRich, SES_FDiv,SES_FEve)%>%
+  group_by(time_code) %>%
+  summarise(total_plots = n(), 
+            mean_SES_FRich = mean(SES_FRich, na.rm = T),
+            std.error.SES_FRich=(sd(SES_FRich, na.rm = T)/sqrt(total_plots)),
+            mean_SES_FDiv = mean(SES_FDiv, na.rm = T),
+            std.error.SES_FDiv=(sd(SES_FDiv, na.rm = T)/sqrt(total_plots)),
+            mean_SES_FEve = mean(SES_FEve, na.rm = T),
+            std.error.SES_FEve=(sd(SES_FEve, na.rm = T)/sqrt(total_plots))) %>%
+  pivot_longer(.,cols = c("mean_SES_FRich","mean_SES_FDiv", "mean_SES_FEve"), names_to = c("type"),
+               values_to= "value") 
+
+SES.data$std.error<-NA
+for(i in 1:nrow(SES.data)){
+  if(grepl("FRich", SES.data$type[i])){
+    SES.data$std.error[i]<-SES.data$std.error.SES_FRich[i]
+  }else{
+    if(grepl("FDiv", SES.data$type[i])){SES.data$std.error[i]<-SES.data$std.error.SES_FDiv[i]}else{
+      SES.data$std.error[i]<-SES.data$std.error.SES_FEve[i]
+    }
+  }
+}  
+
+ggplot(SES.data) +
+  geom_rect(xmin=17652,
+            xmax=17806, ymin=-Inf, ymax=Inf, fill="lightblue", alpha=0.03) +
+  geom_rect(xmin=18017,
+            xmax=Inf, ymin=-Inf, ymax=Inf, fill="lightblue", alpha=0.03) +
+  
+  geom_line(aes(x= (time_code), y = value), size = 1.5) +
+  geom_errorbar(aes(x = time_code, ymin = value-std.error,
+                    ymax = value+std.error), width = 0.5)+
+  geom_point(aes(x= (time_code), y = value), 
+             pch = 21, size = 3, fill = "white",stroke = 2.5) +
+  facet_wrap(~type, scales = "free_y", nrow = 3) +
+  geom_hline(yintercept = 0, lty = 1) +
+  theme_bw()
 
 ##quick models to assess functional metrics
 #June to October Wet Season
@@ -343,13 +433,13 @@ for(i in 1:nrow(trait_table_all)){
 
 hist(trait_table_all$FEve)
 obs<-seq(nrow(trait_table_all)) #observation level random effect
-
+trait_table_all<-trait_table_all %>%
+  mutate(SES_FRich = (mean(FRich, na.rm = T)- mean(mean_FRich_Null, na.rm = T))/sd_FRich_Null,
+         SES_FDiv = (mean(FDiv,na.rm = T)- mean(mean_FDiv_Null,na.rm = T))/sd_FDiv_Null,
+         SES_FEve = (mean(FEve,na.rm = T)- mean(mean_FEve_Null,na.rm = T))/sd_FEve_Null)
 write.csv(trait_table_all,"Data/model_data.csv")
 
 
-ses.mpd.traits = picante::ses.mpd(time1_comm, trait_matrix, null.model = "taxa.labels",
-                                  abundance.weighted = TRUE, runs = 500)
-ses.mpd.traits
 
 
 
@@ -368,312 +458,3 @@ ses.mpd.traits
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-#############################
-#############################
-###treated plots#############################
-#############################
-#############################
-df_t<-df %>%
-  filter(Treatment == "T")
-df_t$time_code<-paste(df_t$Month,df_t$Year,  sep = " ")
-df_t$time_code<-as.Date(as.yearmon(df_t$time_code))
-
-
-#get species list to make trait dta.frame
-species_list<-df_t %>% dplyr::select(! c(Year, Site, Month, Treatment)) %>%
-  pivot_longer(cols = -c("time_code"), names_to = "Species", values_to="abund") %>%
-  dplyr::select(Species)
-species_list<-data.frame(species= unique(species_list$Species))
-
-
-species_list1<-left_join(species_list, dft2, by = c("species" = "Genus.Species") )
-species_list1<-species_list1%>%mutate_at(vars(matches("length")),funs(as.numeric))
-
-
-#here we calculate the traits and then divide by body size to correct for body size
-species_list1$WL<-(species_list1$Weber.s.length_1+species_list1$Weber.s.length_2)/2
-species_list1$HW<-((species_list1$Head.width_1+species_list1$Head.width_2)/2)/species_list1$WL
-species_list1$EL<-((species_list1$Eye.length_1+species_list1$Eye.length_2)/2)/species_list1$WL
-species_list1$ML<-((species_list1$Mandible.length_1+species_list1$Mandible.length_2)/2)/species_list1$WL
-
-species_traits<-species_list1 %>% dplyr::select(species, WL, HW,EL,ML)
-
-#we need species list for everytime code and site
-#then we will try calculating FD for each timecode
-#this will be how we set up a pipeline to calculate FD for every site for every time_code
-#this is used to weight the trait values by abundance
-
-by_sp<-df_t %>% dplyr::select(! c(Year, Month, Treatment)) %>%
-  pivot_longer(cols = -c("time_code", "Site"), names_to = "Species", values_to="abund") %>%
-  mutate(genus = str_extract(Species, "^[\\w]+")) %>%
-  group_by(time_code, Site,Species) %>%
-  summarise(total = sum(abund)) %>%
-  left_join(.,species_traits, by = c("Species"="species")) %>%
-  ungroup() %>%
-  filter(time_code == "2018-04-01") %>%
-  filter(!total == 0) %>%
-  group_by(Species) %>%
-  mutate(total_n = sum(total)) %>%
-  filter(!duplicated(Species)) %>%
-  group_by(Species) %>%
-  arrange(Species) %>%
-  ungroup()
-
-
-#now we have the traits
-species_list<-by_sp$Species
-time1_traits<-by_sp %>% dplyr::select(WL,HW,EL,ML)
-rownames(time1_traits)<-species_list
-#now we have the comm data
-
-time1_comm <- df_c %>% filter(time_code == "2018-04-01") 
-site_names<-time1_comm$Site
-time1_comm<-time1_comm %>%
-  dplyr::select(-c(Year, Site, Month, Treatment,time_code))
-time1_comm = time1_comm[,colSums(time1_comm) > 0]
-rownames(time1_comm)<-site_names
-
-#rescale trait data
-trait_data = scale(time1_traits)
-library(vegan)
-trait_matrix = vegan::vegdist(trait_data,"euclidean")
-branch_lengths<-hclust(trait_matrix,"average")
-branch_lengths
-library(picante)
-trait_tree = ape::as.phylo(branch_lengths)
-plot(trait_tree)
-str(time1_comm)
-FD<-picante::pd(time1_comm, trait_tree)
-vegan_tbl<-data.frame(Site = as.vector(rownames(FD)), SR = FD$SR, FD = FD$PD)
-
-#calculate functional richness
-trial<-dbFD(time1_traits, time1_comm)
-trial$FRic
-trial$FEve
-trial$FDiv
-
-#program to get all fmetrics
-
-trait_table_all_t<-df_t %>% dplyr::select(! c(Year, Month, Treatment)) %>%
-  pivot_longer(cols = -c("time_code", "Site"), names_to = "Species", values_to="abund") %>%
-  mutate(genus = str_extract(Species, "^[\\w]+")) %>%
-  group_by(time_code, Site) %>%
-  summarise(total = sum(abund))
-
-time_stamp_list<-unique(df_t$time_code)
-trait_table_all_t$FD_veg<-NA
-trait_table_all_t$SR<-NA
-trait_table_all_t$FRich<-NA
-trait_table_all_t$FEve<-NA
-trait_table_all_t$FDiv<-NA
-
-
-for(i in 1:length(time_stamp_list)){
-  
-  by_sp<-df_t %>% dplyr::select(! c(Year, Month, Treatment)) %>%
-    pivot_longer(cols = -c("time_code", "Site"), names_to = "Species", values_to="abund") %>%
-    mutate(genus = str_extract(Species, "^[\\w]+")) %>%
-    group_by(time_code, Site,Species) %>%
-    summarise(total = sum(abund)) %>%
-    left_join(.,species_traits, by = c("Species"="species")) %>%
-    ungroup() %>%
-    filter(time_code == time_stamp_list[i]) %>%
-    filter(!total == 0) %>%
-    group_by(Species) %>%
-    mutate(total_n = sum(total)) %>%
-    filter(!duplicated(Species)) %>%
-    group_by(Species) %>%
-    arrange(Species) %>%
-    ungroup()
-  #now we have the traits
-  species_list<-by_sp$Species
-  time1_traits<-by_sp %>% dplyr::select(WL,HW,EL,ML)
-  rownames(time1_traits)<-species_list
-  #now we have the comm data
-  time1_comm <- df_t %>% filter(time_code == time_stamp_list[i]) 
-  site_names<-time1_comm$Site
-  time1_comm<-time1_comm %>%
-    dplyr::select(-c(Year,Site, Month, Treatment,time_code)) 
-  rownames(time1_comm)<-site_names
-  time1_comm = time1_comm[,colSums(time1_comm) > 0]
-  time1_comm = time1_comm[rowSums(time1_comm)> 0, ]
-  #remove species removed from comm from traits
-  
-  
-  #rescale trait data
-  trait_data = scale(time1_traits)
-  library(vegan)
-  trait_matrix = vegan::vegdist(trait_data,"euclidean")
-  branch_lengths<-hclust(trait_matrix,"average")
-  branch_lengths
-  library(picante)
-  trait_tree = ape::as.phylo(branch_lengths)
-  plot(trait_tree)
-  str(time1_comm)
-  FD<-picante::pd(time1_comm, trait_tree)
-  FD
-  vegan_tbl<-data.frame(Site = as.vector(rownames(FD)), SR = FD$SR, FD = FD$PD)
-  
-  for(j in 1:nrow(vegan_tbl)){
-    trait_table_all_t$SR[trait_table_all_t$Site == paste(vegan_tbl$Site[j]) &
-                           trait_table_all_t$time_code == time_stamp_list[i]]<-vegan_tbl$SR[j]
-    trait_table_all_t$FD_veg[trait_table_all_t$Site == paste(vegan_tbl$Site[j]) &
-                               trait_table_all_t$time_code == time_stamp_list[i]]<-vegan_tbl$FD[j]
-  }
-  
-  
-  #calculate functional richness
-  trial<-dbFD(time1_traits, time1_comm)
-  func_rich_metrics<-data.frame(Site = rownames(data.frame(trial)), FRic = trial$FRic,
-                                FEve = trial$FEve, FDiv = trial$FDiv)
-  
-  for(k in 1:nrow(func_rich_metrics)){
-    trait_table_all_t$FRich[trait_table_all_t$Site == paste(func_rich_metrics$Site[k]) &
-                              trait_table_all_t$time_code == time_stamp_list[i]]<-func_rich_metrics$FRic[k]
-    trait_table_all_t$FEve[trait_table_all_t$Site == paste(func_rich_metrics$Site[k]) &
-                             trait_table_all_t$time_code == time_stamp_list[i]]<-func_rich_metrics$FEve[k]
-    trait_table_all_t$FDiv[trait_table_all_t$Site == paste(func_rich_metrics$Site[k]) &
-                             trait_table_all_t$time_code == time_stamp_list[i]]<-func_rich_metrics$FDiv[k]
-  }
-  
-  print(paste(i, "has finished", " "))
-}
-trait_table_all_t$Treatment<-rep("Removal", nrow(trait_table_all_t))
-trait_table_all$Treatment<-rep("Control", nrow(trait_table_all))
-trait_table_full<-rbind(trait_table_all_t,trait_table_all)
-
-
-trait_table_full%>% group_by(time_code, Treatment) %>%
-  summarise(mean_frich = mean(SR, na.rm = T)) %>%
-  ggplot(., aes(x= (time_code), y = mean_frich, color = Treatment)) + 
-  geom_line() +
-  geom_point(pch = 21, size = 3, fill = "white",stroke = 2.5) + theme_bw()
-
-trait_table_full%>% 
-  ggplot(., aes(x= as.factor(time_code), y = FDiv, color = Treatment)) + 
-  geom_boxplot()
-
-
-
-###get total abundances by time_code by genus
-
-by_genus<-df_c %>% dplyr::select(! c(Year, Site, Month, Treatment)) %>%
-  pivot_longer(cols = -c("time_code"), names_to = "Species", values_to="abund") %>%
-  mutate(genus = str_extract(Species, "^[\\w]+")) %>%
-  group_by(time_code, genus) %>%
-  summarise(total = sum(abund)) %>%
-  filter(total > 0)
-
-
-which_genera<-data.frame(generalist = unique(by_genus$genus), keep = NA)
-
-for(i in 1:nrow(which_genera)){
-  if(length(unique(by_genus$time_code[by_genus$genus==which_genera[i,1]]))> 7){
-    which_genera$keep[i]<-"yes"
-  }else{
-    which_genera$keep[i]<-"no"
-  }
-}
-
-which_genera_keep<-which_genera %>% filter(keep == "yes") %>%
-  dplyr::select(generalist)
-
-by_genus1<-by_genus %>% filter(genus %in% which_genera_keep$generalist)
-ggplot(data = NULL) +
-  geom_line(data = by_genus1, aes(x = time_code, y = log(total), color = genus),
-            alpha = 1, size = 1.5) 
-
-#' you can look at peaks and vallyes based on genus
-#' you can identify maximum overlap time periods
-#' how does morphospace change across wet and dry seasons?
-
-
-
-
-
-
-average_by_genus<-df_c %>% dplyr::select(! c(Year, Site, Month, Treatment)) %>%
-  pivot_longer(cols = -c("time_code"), names_to = "Species", values_to="abund") %>%
-  mutate(genus = str_extract(Species, "^[\\w]+")) %>%
-  group_by(time_code) %>%
-  summarise(total = mean(abund)) 
-
-ggplot(data = NULL) +
-  geom_line(data = by_genus, aes(x = time_code, y = log(total), group = genus),
-            alpha = 0.4) +
-  geom_line(data = average_by_genus, aes(x = time_code, y = log(total)), color = "blue",
-            size = 2)
-
-
-
-
-
-plot_data<-data.frame(matrix(NA, nrow = nrow(callingcard), ncol = 4))
-
-
-colnames(plot_data)<-c("timestampcode", "Add.beta", "multi_beta", "beta_part")
-
-for(i in 1:nrow(callingcard)){
-  data_analysis<-df_c %>% filter(Year == callingcard[i,1] & Month == callingcard[i,2]) %>%
-    select(-Year, -Site, -Month, -Treatment)
-  alpha = vegan::specnumber(data_analysis)
-  spN_allplots = colSums(data_analysis)
-  gamma = length(which(spN_allplots > 0))
-  plot_data$timestampcode[i]<-paste(callingcard[i,1], callingcard[i,2], sep = "_")
-  plot_data$Add.beta[i] = gamma - mean(alpha)
-  plot_data$multi_beta[i] = gamma/mean(alpha)
-  plot_data$beta_part[i]= 1 - mean(alpha)/gamma
-  
-}
-
-plot_data$time_period<-seq(1:14)
-plot_data$Treatment<-rep("C",14)
-
-#do this with treatment
-df_c<-df %>%
-  filter(Treatment == "T")
-
-
-df_c %>% filter()
-
-callingcard<-unique(df_c[,c("Year", "Month")])
-
-
-
-plot_data2<-data.frame(matrix(NA, nrow = nrow(callingcard), ncol = 4))
-colnames(plot_data2)<-c("timestampcode", "Add.beta", "multi_beta", "beta_part")
-
-for(i in 1:nrow(callingcard)){
-  data_analysis<-df_c %>% filter(Year == callingcard[i,1] & Month == callingcard[i,2]) %>%
-    select(-Year, -Site, -Month, -Treatment)
-  alpha = vegan::specnumber(data_analysis)
-  spN_allplots = colSums(data_analysis)
-  gamma = length(which(spN_allplots > 0))
-  plot_data2$timestampcode[i]<-paste(callingcard[i,1], callingcard[i,2], sep = "_")
-  plot_data2$Add.beta[i] = gamma - mean(alpha)
-  plot_data2$multi_beta[i] = gamma/mean(alpha)
-  plot_data2$beta_part[i]= 1 - mean(alpha)/gamma
-  
-}
-
-plot_data2$time_period<-seq(1:14)
-plot_data2$Treatment<-rep("T",14)
-plot_data
-plot_data2
-
-#rbind
-hello_world<-rbind(plot_data,plot_data2)
-
-ggplot(hello_world) +
-  geom_point(aes(x = time_period, y = beta_part, color =Treatment))
